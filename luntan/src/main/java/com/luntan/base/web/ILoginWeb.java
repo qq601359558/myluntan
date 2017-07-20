6c0b252e6aad06aaab40f3b06ecdd1009a10e0ff
ThinkPad <ThinkPad@ThinkPad-PC>
2017-07-20 17:28:04 +0800

    a

---
+++ luntan/src/main/java/com/luntan/base/web/ILoginWeb.java
@@ -0,0 +1,7 @@
+package com.luntan.base.web;
+import com.luntan.base.bo.LoginInfo;
+
+public interface ILoginWeb {
+	/**µÇÂ¼*/
+	public String login(LoginInfo loginInfo);
+}
---
+++ luntan/src/main/java/com/luntan/base/web/LoginWeb.java
@@ -0,0 +1,27 @@
+package com.luntan.base.web;
+
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luntan.base.bo.LoginInfo;
+import com.luntan.base.bo.User;
+import com.luntan.base.service.IBaseService;
+
+@Controller
+public class LoginWeb implements ILoginWeb {
+	private static Logger logger=Logger.getLogger(LoginWeb.class);
+	@Resource
+	private IBaseService baseService;
+	@RequestMapping("login.so")
+	public String login(LoginInfo loginInfo) {
+		
+		return "base/login";
+	}
+
+}
